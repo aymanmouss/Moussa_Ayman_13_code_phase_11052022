9c5b379e66d409eb3439f2726a8c5d0766f16f8a
aymanmouss <ayman@moussa.uk>
2022-05-11 23:34:26 +0200

    init

--- argent-bank/src/compoenents/Header/index.jsx
+++ argent-bank/src/compoenents/Header/index.jsx
@@ -12,7 +12,7 @@ function Header() {
       <div>
         <a class='main-nav-item' href='./sign-in.html'>
           <i class='fa fa-user-circle'></i>
-          Sign In
+          Sign Inssss
         </a>
       </div>
     </nav>
